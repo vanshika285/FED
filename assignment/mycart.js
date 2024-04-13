fe9d6d29cf8c3c1c84e262d3b3f40086cbab9e2c
vanshika285 <156657046+vanshika285@users.noreply.github.com>
2024-04-13 14:21:00 +0530

    uploading major projects and assignments

---
+++ assignment/mycart.js
@@ -0,0 +1,82 @@
+const product = [
+    {
+        id: 0,
+        image: 'C:\Users\shabn\OneDrive\Desktop\flipkart dress 1.webp',
+        title: '',
+        price: 1800,
+    },
+    {
+        id: 1,
+        image:'C:\Users\ASUS\Downloads\download.jpeg',
+        title: 'Air Pods Pro',
+        price: 1500,
+    },
+    {
+        id: 2,
+        image: '',
+        title: '250D DSLR Camera',
+        price: 2230,
+    },
+    {
+        id: 3,
+        image: '',
+        title: 'Head Phones',
+        price: 1000,
+    }
+];
+const categories = [...new Set(product.map((item)=>
+    {return item}))]
+    let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+    var {image, title, price} = item;
+    return(
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2>$ ${price}.00</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+        `</div>
+        </div>`
+    )
+}).join('')
+
+var cart =[];
+
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
+}
+function delElement(a){
+    cart.splice(a, 1);
+    displaycart();
+}
+
+function displaycart(){
+    let j = 0, total=0;
+    document.getElementById("count").innerHTML=cart.length;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "$ "+0+".00";
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
+            total=total+price;
+            document.getElementById("total").innerHTML = "$ "+total+".00";
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+            );
+        }).join('');
+    }
+}
